25e897e9d5bfef934c659bae4302f0cdd9423fc6
unoguerta <unoguerta@gmail.com>
2023-11-06 20:32:14 +0800

    edits
    edited error messages
    fixed shipping.php logic error
    button in progress

--- PHP_Webpage/shipping.php
+++ PHP_Webpage/shipping.php
@@ -59,6 +59,7 @@ if (isset($bookArray['bookName'])) {
         <h1><a href="../PHP_Webpage/home.php">DUNOT</a></h1>
         <span class="icons">
           <a href="../PHP_Webpage/catalogue.php"><i class="fa fa-book fa-2x" aria-hidden="true"></i></a>
+
           <?php
           if ($cartNum > 0) {
             echo "<div class='shoppingBag' data-count='" . $cartNum . "'>";
@@ -66,12 +67,12 @@ if (isset($bookArray['bookName'])) {
             echo "<div class='shoppingBag'>";
           }
           ?>
+          <!-- <div class="shoppingBag"> -->
           <a href="../PHP_Webpage/basket.php"><i class="fa fa-shopping-bag fa-2x" aria-hidden="true"></i></a>
-    
+    </div>
     <a href="../PHP_Webpage/logout.php"><i class="fa fa-sign-out fa-2x" aria-hidden="true"></i></a>
     </span>
     </nav>
-    </div>
   </div>
 
   <div class="container">
@@ -107,7 +108,7 @@ if (isset($bookArray['bookName'])) {
             <input type="text" id="postalcode" class="input_box" placeholder="Postal Code" name="postalCode" required />
             <span id="pcError" class="error" style="color: red;"></span>
           </div>
-          <button type="submit" class="btn_submit" id="submitButton" disabled>Ready to Ship</button>
+          <button type="submit" class="btn_submit_2" id="submitButton" disabled>Ready to Ship</button>
         </form>
       </div>
       <div class="column_6">
@@ -149,18 +150,18 @@ if (isset($bookArray['bookName'])) {
 
     document.getElementById('email').addEventListener('input', function () {
       const emailInput = this.value;
-      const emailRegex = /^(?:[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}|@localhost)$/;
+      const emailRegex = /^(?:[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,4}|[a-zA-Z0-9._-]+@localhost)$/;
 
       const emailError = document.getElementById('emailError');
       const submitBtn = document.getElementById('submitButton');
-      
+
 
       if (emailRegex.test(emailInput)) {
         emailError.textContent = '';
         submitBtn.removeAttribute('disabled');
         checkEmail = true;
       } else {
-        emailError.textContent = 'Invalid email format';
+        emailError.textContent = 'Invalid email format, please follow the format : youremail@mailcompany.com, yourname@mail.co';
         submitBtn.setAttribute('disabled', true);
         checkEmail = false;
       }
@@ -168,17 +169,17 @@ if (isset($bookArray['bookName'])) {
 
     document.getElementById('country').addEventListener('input', function () {
       const countryInput = this.value;
-      const countryRegex = /^[A-Za-z]+$/;
+      const countryRegex = /^[A-Za-z ]+$/;
 
       const countryError = document.getElementById('countryError');
       const submitBtn = document.getElementById('submitButton');
-      
+
       if (countryRegex.test(countryInput)) {
         countryError.textContent = '';
         submitBtn.removeAttribute('disabled');
         checkCountry = true;
       } else {
-        countryError.textContent = 'Invalid country';
+        countryError.textContent = 'Invalid country, can only contain letters and spaces';
         submitBtn.setAttribute('disabled', true);
         checkCountry = false;
       }
@@ -190,14 +191,14 @@ if (isset($bookArray['bookName'])) {
 
       const nameError = document.getElementById('nameError');
       const submitBtn = document.getElementById('submitButton');
-      
+
 
       if (nameRegex.test(nameInput)) {
         nameError.textContent = '';
         submitBtn.removeAttribute('disabled');
         checkName = true;
       } else {
-        nameError.textContent = 'Invalid name';
+        nameError.textContent = 'Invalid name, can only contain letters and spaces';
         submitBtn.setAttribute('disabled', true);
         checkName = false;
       }
@@ -209,15 +210,15 @@ if (isset($bookArray['bookName'])) {
 
       const phoneError = document.getElementById('phoneError');
       const submitBtn = document.getElementById('submitButton');
-      
+
       if (phoneRegex.test(phoneInput)) {
         phoneError.textContent = '';
         submitBtn.removeAttribute('disabled');
         checkPhone = true;
       } else {
-        phoneError.textContent = 'Invalid phone number';
+        phoneError.textContent = 'Invalid phone number, only allow 8 digits and numbers only';
         submitBtn.setAttribute('disabled', true);
-        checkName = false;
+        checkPhone = false;
       }
     });
 
@@ -227,28 +228,28 @@ if (isset($bookArray['bookName'])) {
 
       const pcError = document.getElementById('pcError');
       const submitBtn = document.getElementById('submitButton');
-      
+
       if (pcRegex.test(pcInput)) {
         pcError.textContent = '';
         submitBtn.removeAttribute('disabled');
         checkPC = true;
       } else {
-        pcError.textContent = 'Invalid postal code';
+        pcError.textContent = 'Invalid postal code, only allow 6 digits and numbers only';
         submitBtn.setAttribute('disabled', true);
         checkPC = false;
       }
     });
     document.addEventListener('keyup', function (event) {
-    const submitBtn = document.getElementById('submitButton');
-    
-    if (checkEmail && checkCountry && checkName && checkPhone && checkPC) {
+      const submitBtn = document.getElementById('submitButton');
+      const total = <?php echo $sumBooks; ?>;
+      if (checkEmail && checkCountry && checkName && checkPhone && checkPC && total !== 0) {
         submitBtn.removeAttribute('disabled');
         console.log('Button enabled');
-    } else {
+      } else {
         submitBtn.setAttribute('disabled', true);
         console.log('Button disabled');
-    }
-});
+      }
+    });
 
 
     <?php
@@ -320,7 +321,7 @@ if (isset($bookArray['bookName'])) {
       }
       //Create Session                
       //$_SESSION['name'] = $userName;            
-     
+    
 
       // exit;   
       $sql->close();
